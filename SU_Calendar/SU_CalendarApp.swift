20aab43c65366f596651d9534fae11a681397de4
swj <swjtwin@naver.com>
2024-03-13 22:54:45 +0900

    Initial Commit

---
+++ SU_Calendar/SU_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SU_CalendarApp.swift
+//  SU_Calendar
+//
+//  Created by woojin Shin on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SU_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
